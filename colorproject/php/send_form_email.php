f0e738e1921f95fb0b32740bfe425a0eac434b98
gaemin <prattegr@gmail.com>
2017-02-21 17:12:18 +0900

    PHP Update

--- colorproject/php/send_form_email.php
+++ colorproject/php/send_form_email.php
@@ -6,7 +6,7 @@ if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
  
-    $email_to = "kai@colorproject.biz";
+    $email_to = "info@colorproject.biz";
  
     $email_subject = $_POST['_subject'];
  
@@ -40,7 +40,7 @@ if(isset($_POST['email'])) {
  
         !isset($_POST['comments'])) {
  
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+        died('We are sorry, but there appears to be a problem with the form you submitted. Did you fill out the form in English? ');       
  
     }
  
@@ -137,11 +137,8 @@ $headers = 'From: '.$email_from."\r\n".
     <meta name="author" content="Kai Gaemin Lee">
 <!-- Favicons
     ================================================== -->
-    <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
-    <link rel="apple-touch-icon" href="../img/apple-touch-icon.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="../img/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="../img/apple-touch-icon-114x114.png">
-
+  <link rel="icon" type="image/png" href="../img/5cclogo.png" >
+  <link href="../img/tablogo.png" rel="shortcut icon">
     <!-- Bootstrap -->
     <link rel="stylesheet" type="text/css"  href="../css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="../fonts/font-awesome/css/font-awesome.css">
@@ -163,7 +160,7 @@ $headers = 'From: '.$email_from."\r\n".
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
-<body>
+  <body>
     <div id="tf-home">
         <div class="overlay">
             <div id="sticky-anchor"></div>
@@ -171,27 +168,22 @@ $headers = 'From: '.$email_from."\r\n".
                 <div class="container">
                     <!-- Brand and toggle get grouped for better mobile display -->
                     <div class="navbar-header">
-                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                      </button>
-                      <a class="navbar-brand logo" href="index.html"> ColorProject :: Inscribe Tones in Colors </a>
+                      <a class="navbar-brand noItalics" ><div class="wiii"><img src="../img/cplogo.png"\ height="50" width="190"></div></a>
                     </div>
 
                     <!-- Collect the nav links, forms, and other content for toggling -->
                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                       <ul class="nav navbar-nav navbar-right">
                         <li><a href="#tf-home">Home</a></li>
-                        <li><a href="#tf-about">About</a></li>
+                        <li><a href="#tf-aboutus">About</a></li>
                         <li><a href="#tf-project">Projects</a></li>
-                        <li><a href="#tf-team">Our Team</a></li>
+                        <!-- <li><a href="#tf-team">Our Team</a></li> -->
                         <li><a href="#tf-contact">Contact</a></li>
                       </ul>
                     </div><!-- /.navbar-collapse -->
                 </div><!-- /.container-fluid -->
             </nav>
+
 
             <div class="container">
                 <div class="content">
